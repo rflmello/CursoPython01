94e028a3b094474dc1d2eca87d19162649c63f39
rafael melo <rafael.melo@sci10.local>
2019-11-04 07:17:26 -0300

    Create operados.py

---
+++ operados.py
@@ -0,0 +1,9 @@
+# == igual
+# != diferente
+# > maior
+# < menor
+# >= maior ou igual
+# <= menor ou igual
+# AND duas condições verdadeiras
+# OR Pelo menos uma condição é verdadeira
+# NOT Inverte o valor
